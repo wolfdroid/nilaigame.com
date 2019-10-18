f3ece72191f186afe4de4f1673bc5c98af32cc46
Im Rafid <36785493+WolfDroid@users.noreply.github.com>
2019-10-18 16:38:55 +0700

    Add files via upload

--- nilaigame/routes/web.php
+++ nilaigame/routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Homepage
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+//Controller Method
+//INDEX
+Route::get('/', 'PagesController@weland');
+Route::get('/signin', 'PagesController@signin');
+Route::post('/Dashboard','loginController@Dashboard');
+Route::post('/Dashboard/insert','loginController@insert');
+//Route::get('/display','loginController@display');
+
+//About
+//Route::get('/about', 'PagesController@about');
+
+
+/*Without Controller
+//About
+Route::get('/about', function () {
+    return view('pages.about');
 });
+*/
+
+
+
+//Dynamic
+/*
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return 'This is user '.$name. 'with ID of ' .$id;
+});
+*/
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
